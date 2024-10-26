3a7c9fce80508cc682b8cf93a7ef4ffdc2d68b4b
stevenbm95 <stevenbm95@gmail.com>
2024-10-25 22:28:19 -0500

    nuevo comit de dist

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 
-import path from 'path'; 
+import path from 'path';
 
 // Cargar manualmente el archivo .env antes de acceder a las variables
 dotenv.config({ path: path.resolve(__dirname, '.env') });
